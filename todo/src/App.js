ddb6cd0303b4e460959af00a70b5fd1ba8e14237
Aali7575 <tafadzwaaaliyahndoro@gmail.com>
2024-03-22 21:04:42 +0200

    managed to intergrate react with tailwind, working perfectly so far

---
+++ todo/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Start from './components/hello'; 
+
+function App() {
+  return (
+    <div className="App">
+      <Start/>
+
+    </div>
+    
+  );
+}
+
+export default App;
---
+++ todo/src/components/hello.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+function Start () {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Tafadzwa</h1>
+    </div>
+  )
+}
+
+export default  Start
