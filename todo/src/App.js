50c595fdaac26b8c7f809c82fca8be13ec5edf99
Aali7575 <tafadzwaaaliyahndoro@gmail.com>
2024-03-22 21:51:00 +0200

    everything working properly now, just too ugly

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,16 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-import Start from './components/hello'; 
-import Todos from './components/todos';
+// import logo from './logo.svg';
+// import './App.css';
+// import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
+// import Start from './components/hello'; 
+// import Todos from './components/todos';
+
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Todos from "./components/todos";
+import Start from "./components/hello";
 
 function App() {
   return (
-    <div className="App">
-      <Start/>
-      <Todos/>
-
-    </div>
-    
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Start />} />
+        <Route path="/todos" element={<Todos />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
